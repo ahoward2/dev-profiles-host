d3808d26a5f52ea4b3d46684fe0474cecb388d33
Austin Howard <ahoward2@unca.edu>
2021-10-05 23:30:29 -0400

    initial project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DynamicRemoteContainer from "./hooks/DynamicRemoteContainer";
+import AppWrapper from "./App.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <AppWrapper>
+    <h1>Developer Profiles</h1>
+    <h3>Built With Micro Frontends</h3>
+    <hr />
+    <div className="profile-container">
+      <DynamicRemoteContainer
+        componentProps={{}}
+        url="http://localhost:3003/remoteEntry.js"
+        scope="DevProfileOne"
+        module="./Profile"
+      />
+      <DynamicRemoteContainer
+        componentProps={{}}
+        url="http://localhost:3001/remoteEntry.js"
+        scope="DevProfileTwo"
+        module="./Profile"
+      />
     </div>
-  );
-}
+  </AppWrapper>
+);
 
 export default App;
